ce3eb7d82d510804a070e4095bef94c524f27acc
Green_Computer <Green_Computer@DESKTOP-912HOCH>
2020-11-19 10:41:58 +0900

    Git commit test in eclipse.

--- AWTMain.java
+++ AWTMain.java
@@ -1,6 +1,12 @@
+import java.awt.Frame;
+
 public class AWTMain {
 	public static void main(String[] args) {
 		System.out.println("AWTMain");
+		Frame frm = new Frame("Cal");
+		frm.setSize(300, 200);
+		frm.setLocation(100, 100);
+		frm.setVisible(true);
 
 	}
 
